fdad160b690b5f8a1c6b8ac9bc8397968ea1c4be
Matthias Lerch <matthias.lerch@adnovum.ch>
2025-12-03 11:52:36 +0100

    wip

---
+++ remove_preinstalled.sh
@@ -0,0 +1,1 @@
+sudo pacman -R 1password-beta 1password-cli aether kdenlive localsend obs-studio obsidian signal-desktop xournalpp typora asdcontrol
--- security/master.sh
+++ security/master.sh
@@ -1,4 +1,10 @@
-sudo pacman --noconfirm -S linux-hardened
+sudo pacman --noconfirm -S linux-hardened linux-hardened-headers
 yay --noconfirm -S apparmor.d-git
 sudo systemctl enable --now apparmor.service
 sudo sed -i 's/"quiet splash"/"quiet splash lsm=landlock,lockdown,yama,integrity,apparmor,bpf"/' /etc/default/limine
+grep '^timeout' /boot/limine.conf || (echo 'timeout: 30' | sudo tee -a /boot/limine.conf > /dev/null)
+
+# firewall
+sudo ufw delete allow 53317/udp
+sudo ufw delete allow 53317/tcp
+yes | sudo ufw delete 1
